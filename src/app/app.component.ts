bbc4034aee29a9ae6564b264160e27f7119f80ed
lironh <lironenk@gmail.com>
2018-11-20 00:07:36 +0200

    :sparkle: tagify angular 7 wrapper! module exports a component and a service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {TagifyService} from '../ng-tagify-wrapper/tagify.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'ngTagify';
+export class AppComponent implements OnDestroy {
+
+  constructor(private tagifyService: TagifyService) {}
+  public settings = { blacklist: ['fucking', 'shit']};
+
+  onAdd(tagify) {
+    console.log('added a tag', tagify);
+  }
+
+  onRemove(tags) {
+    console.log('removed a tag', tags);
+  }
+  clearTags() {
+    this.tagifyService.removeAll();
+  }
+  addTags() {
+    this.tagifyService.addTags(['this', 'is', 'cool']);
+  }
+  ngOnDestroy() {
+    this.tagifyService.destroy();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {TagifyModule} from '../ng-tagify-wrapper';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    TagifyModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
