2de4266fbc81433bcd00b3160b851bebdd7cb910
Brakebein <bruschie@hotmail.com>
2021-01-04 00:31:49 +0100

    improve demo app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
-import {Component, OnDestroy} from '@angular/core';
-import {TagifyService, TagifySettings} from 'ngx-tagify';
+import { Component } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+// import { TagifyService, TagifySettings } from 'ngx-tagify';
+import { TagifyService, TagifySettings } from '../../projects/ngx-tagify/src/public-api';
 
 
 @Component({
@@ -7,9 +9,24 @@ import {TagifyService, TagifySettings} from 'ngx-tagify';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnDestroy {
+export class AppComponent {
 
-  public settings: TagifySettings = { blacklist: ['fucking', 'shit']};
+  private valuesData: {value: string}[] = [{ value: 'Super' }];
+
+  get values(): {value: string}[] {
+    return this.valuesData;
+  }
+
+  set values(v: {value: string}[]) {
+    this.valuesData = v;
+  }
+
+  public settings: TagifySettings = {
+    placeholder: 'Start typing...',
+    blacklist: ['fucking', 'shit']
+  };
+
+  suggestion$ = new BehaviorSubject<string[]>(['Hello', 'World']);
 
   constructor(private tagifyService: TagifyService) { }
 
@@ -20,13 +37,21 @@ export class AppComponent implements OnDestroy {
   onRemove(tags) {
     console.log('removed a tag', tags);
   }
+
   clearTags() {
     this.tagifyService.get('test').removeAllTags();
   }
+
   addTags() {
     this.tagifyService.get('test').addTags(['this', 'is', 'cool']);
   }
-  ngOnDestroy() {
-    this.tagifyService.destroy('test');
+
+  replaceTags() {
+    this.values = [
+      {value: 'this'},
+      {value: 'is'},
+      {value: 'awesome'}
+    ];
   }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import {TagifyModule} from 'ngx-tagify';
+// import { TagifyModule } from 'ngx-tagify';
+import { TagifyModule } from '../../projects/ngx-tagify/src/public-api';
 
 @NgModule({
   declarations: [
